1647a4751eaf680a7e93a70e6a71d954f2e945ae
S1nourish <53213447+S1nourish@users.noreply.github.com>
2021-01-21 10:12:01 +0100

    Add files via upload

---
+++ front/src/sidebar.js
@@ -0,0 +1,193 @@
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./css/sidebar.css";
+import TomatoLogo from "./assets/tomato.png";
+import {
+    Apps,
+    Info,
+    Star,
+    Games,
+    DeleteOutline,
+    BarChart,
+    LibraryBooks,
+    FormatListNumbered,
+} from "@material-ui/icons";
+import {
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+} from "@material-ui/core";
+import serverConv from "./data/serverConv";
+import { SearchHistoryContext } from "./context";
+
+const LINKS = [
+    { url: "/", title: "Home", Icon: Apps },
+    { url: "/tank-stats", title: "Tank Stats", Icon: BarChart },
+    {
+        url: "/leaderboards",
+        title: "Leaderboards",
+        Icon: FormatListNumbered,
+    },
+    // {url: "/tank-list", title: "Tank List", Icon: BarChart},
+    // { url: "/server-stats", title: "Server Stats", Icon: Public },
+    { url: "/moe", title: "MoE Reqs", Icon: Star },
+    { url: "/mastery", title: "Mastery Reqs", Icon: Star },
+    { url: "/wn8", title: "WN8 Exp. Values", Icon: Games },
+    {
+        url: "/stats-reference",
+        title: "Stats Reference",
+        Icon: LibraryBooks,
+    },
+    { url: "/about", title: "About", Icon: Info },
+];
+
+const Sidebar = withRouter((props) => {
+    const { history, clearHistory } = useContext(SearchHistoryContext);
+    const redirectToPlayerStatsPage = (playerName, playerID, playerServer) => {
+        props.history.push(
+            `/stats/${serverConv[playerServer]}/${playerName}=${playerID}`
+        );
+    };
+
+    return (
+        <div className="sidebar">
+            <div className="layer">
+                <Link to="/">
+                    <div className="logo">
+                        <img
+                            src={TomatoLogo}
+                            alt="logo"
+                            style={{
+                                height: "110%",
+                                width: "auto",
+                                display: "flex",
+                                alignItems: "center",
+                                padding: "0rem",
+                            }}
+                        />
+                    </div>
+                </Link>
+                <div className="line" />
+                <div className="menu">
+                    {LINKS.map(({ url, title, Icon }) => (
+                        <Link to={url} className="menu-link" key={url}>
+                            <Icon className="menu-link-icon" />
+                            &nbsp;&nbsp;{title}
+                        </Link>
+                    ))}
+                </div>
+                <div className="line" />
+                <div className="menu">
+                    <Typography
+                        variant="h6"
+                        className="menu-link"
+                        style={{ marginTop: "-5px", paddingRight: "unset" }}
+                    >
+                        Recent searches
+                        <IconButton
+                            aria-label="clear history"
+                            size="small"
+                            onClick={clearHistory}
+                            style={{
+                                color: "inherit",
+                                marginLeft: ".6rem",
+                                marginBottom: ".1rem",
+                            }}
+                        >
+                            <DeleteOutline />
+                        </IconButton>
+                    </Typography>
+                    <List
+                        aria-label="recent searches"
+                        style={{ marginTop: "-10px" }}
+                    >
+                        {history
+                            .slice(0, Math.min(history.length, 5)) // No more than 5 recent searches
+                            .map(({ name, id, server }) => (
+                                <ListItem
+                                    key={id}
+                                    button
+                                    onClick={() =>
+                                        redirectToPlayerStatsPage(
+                                            name,
+                                            id,
+                                            server
+                                        )
+                                    }
+                                    style={{ padding: "0px 20px 10px 22px" }}
+                                >
+                                    <img
+                                        src={require(`./assets/flagIcons/${server}mini.png`)}
+                                        style={{ maxHeight: "21px" }}
+                                        alt={"4"}
+                                    />
+                                    <ListItemText
+                                        style={{
+                                            marginLeft: "10px",
+                                            color: "white",
+                                        }}
+                                        primary={name}
+                                    />
+                                </ListItem>
+                            ))}
+                    </List>
+                </div>
+                <div
+                    style={{
+                        fontSize: "0.8rem",
+                        fontWeight: "300",
+                        color: "#96a7c7",
+                        padding: "15px",
+                        bottom: "90px",
+                        position: "absolute",
+                    }}
+                >
+                    <span style={{fontWeight: "500"}}>Partnered Websites</span> 
+                    <span style={{color: "white"}}>
+                        <br />
+                        <a  style={{color: "white"}}
+                            target="blank"
+                            href="#"
+                        >
+                            herhor.net
+                        </a>
+                    </span>
+                     - MoE rankings 
+                    <br />
+                </div>
+                <div 
+                    className="line"  
+                    style={{
+                        bottom: "90px",
+                        position: "absolute",
+                }} />
+
+                <div
+                    style={{
+                        fontSize: "0.8rem",
+                        fontWeight: "300",
+                        color: "#96a7c7",
+                        padding: "15px",
+                        bottom: "0px",
+                        position: "absolute",
+                    }}
+                >
+                    Website created by{" "}
+                    <Link
+                        style={{ color: "rgb(205, 205, 205)" }}
+                        to="#"
+                    >
+                        _The_Last_Emperor_
+                    </Link>{" "}
+                    and is not affiliated with Wargaming.net.
+                    <br />
+                    Zeyu Yang © 2021
+                </div>
+            </div>
+        </div>
+    );
+});
+
+export default Sidebar;
---
+++ front/src/tomatopedia.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+import Topbar from "./topbar";
+import Sidebar from "./sidebar";
+import Search from "./search";
+import About from "./about";
+import TankStatsPage from "./statpages/tankStatsPage";
+import StatsPage from "./statpages/statsPage";
+import ServerStatsPage from "./statpages/serverStatsPage";
+import TankPage from "./statpages/tankPage";
+import StatsReference from "./statpages/statsReference";
+import Leaderboards from "./statpages/leaderboards";
+import WN8Expected from "./statpages/wn8expected";
+import MoEExpected from "./statpages/MoEPage";
+import MasteryExpected from "./statpages/masteryPage";
+
+import "./css/body.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeContext } from "./context";
+const trackingId = process.env.REACT_APP_GA;
+
+export default function Tomatopedia() {
+    useEffect(() => {
+        ReactGA.initialize(trackingId);
+        ReactGA.pageview("/");
+    }, []);
+
+    const { theme } = React.useContext(ThemeContext);
+
+    return (
+        <Router>
+            <div className={theme === "dark" ? "dark-mode" : "light-mode"}>
+                <Sidebar />
+                <Topbar />
+                <main className="wrapper">
+                    <Switch>
+                        <Route path="/stats/:server/:user">
+                            <StatsPage />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/server-stats">
+                            <ServerStatsPage />
+                        </Route>
+                        <Route path="/tank-stats">
+                            <TankStatsPage />
+                        </Route>
+                        <Route path="/stats-reference">
+                            <StatsReference />
+                        </Route>
+                        <Route path="/leaderboards">
+                            <Leaderboards />
+                        </Route>
+                        <Route path="/tank">
+                            <TankPage />
+                        </Route>
+                        <Route path="/wn8">
+                            <WN8Expected />
+                        </Route>
+                        <Route path="/moe">
+                            <MoEExpected />
+                        </Route>
+                        <Route path="/mastery">
+                            <MasteryExpected />
+                        </Route>
+                        <Route exact path="/">
+                            <Search />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    );
+}
